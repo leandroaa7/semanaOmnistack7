3eadea6549339308770dd386efecf83e07b233f3
leandroaa7 <leandroaalexandre@gmail.com>
2020-03-30 20:26:50 -0300

    aula2

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const connectionString =
     "mongodb+srv://semana:semana@cluster0-pligr.mongodb.net/test?retryWrites=true&w=majority"
@@ -11,5 +16,16 @@ const connectionParams = {
 }
 mongoose.connect(connectionString, connectionParams);
 
+//socket middleware
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, "..", "uploads", "resized")));
+
 app.use(require('./routes'));
-app.listen(3333);
+
+server.listen(3333);
